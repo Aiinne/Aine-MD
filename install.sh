5fc220cd8a52aeb9227f0feae01eba1b0971b6f4
Aine <79745902+Aiinne@users.noreply.github.com>
2022-05-30 21:04:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,24 @@
+#!/usr/bin/bash
+
+pkg upgrade -y
+pkg update -y
+pkg install nodejs -y
+pkg clean
+pkg install tty-clock -y
+pkg install sox -y
+pkg install jq -y
+pkg install mc -y
+pkg install figlet -y
+pkg install wget -y
+pkg install yarn
+pkg install ffmpeg
+pkg install libwebp -y
+pkg install nodejs -y
+pkg install tesseract -y
+pkg install imagemagick -y
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+yarn
+
 echo ":::::::::::::::::::::::::::::::::"
 echo ":::::::::::::::::::::::::::::::::"
 echo ":::::         Aine          :::::"
