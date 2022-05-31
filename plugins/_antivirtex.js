3f08d4d31479d668599b58a751e2ecebeab14c8b
Aine <79745902+Aiinne@users.noreply.github.com>
2022-05-31 16:40:06 +0700

    Create _antivirtex.js
    Tambahkan sendiri code virusnya

---
+++ plugins/_antivirtex.js
@@ -0,0 +1,31 @@
+let fs = require('fs')
+let { MessageType } = require('@adiwajshing/baileys')
+let handler = m => m
+
+handler.all = async function (m, { isBlocked }) {
+    if (isBlocked) return
+
+   let regs = /(ผิดุท้เึางืผิดุท้เึางื)/i
+   let isVertexThai = regs.exec(m.text)
+   if (isVertexThai && !m.fromMe) {
+   conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   
+   let regk = /(♚㜸ཽཽࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧ͢͢㜺ࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧ㜸ཽཽཽ͢͢͢♚)/i
+   let isVertexSymbol = regk.exec(m.text)
+   if (isVertexThai && !m.fromMe) {
+   conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   
+   let regp = /(๒๒)/i
+   let isVertexsThai= regp.exec(m.text)
+   if (isVertexsThai && !m.fromMe) {
+   conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+   }
+   
+   // tambahin sendiri code virus WhatsApp..
+}
+
+handler.botAdmin = true
+handler.group = true
+module.exports = handler
