e5798af990767eba2464a3071880bf79e3dacbb9
Aine <79745902+Aiinne@users.noreply.github.com>
2022-06-01 12:14:17 +0700

    Update pengumuman.js
    Biar ke tag pakai all media :v

--- plugins/pengumuman.js
+++ plugins/pengumuman.js
@@ -1,4 +1,4 @@
-/*let { generateWAMessageFromContent } = require('@adiwajshing/baileys')
+let { generateWAMessageFromContent } = require('@adiwajshing/baileys')
 let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.id)
   let q = m.quoted ? m.quoted : m
@@ -26,28 +26,12 @@ let handler = async (m, { conn, text, participants }) => {
     messageId: msg.key.id
   })
 }
-handler.help = ['hidetag', 'pengumuman'].map(v => v + ' [teks]')
-handler.tags = ['group']
-handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
-
-handler.group = true
-handler.admin = true
-handler.limit = true
-
-module.exports = handler*/
-
-const { MessageType } = require('@adiwajshing/baileys')
-
-let handler = async (m, { conn, text, participants }) => {
-	let q = text
-conn.sendMessage(m.chat, { text : q ? q : '' , mentions: participants.map(a => a.id) }, )
-}
-
 handler.help = ['hidetag'].map(v => v + ' [teks]')
 handler.tags = ['group']
 handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
-handler.limit = true
+
 handler.group = true
 handler.admin = true
+handler.limit = true
 
 module.exports = handler
