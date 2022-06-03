bf279f9afa57fef158ac6d04a11a1d3975b09013
Aine <79745902+Aiinne@users.noreply.github.com>
2022-06-03 20:19:35 +0700

    Update kecil

--- plugins/broadcastchats.js
+++ plugins/broadcastchats.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
   let teks = text ? text : cc.text
   conn.reply(m.chat, `_Mengirim pesan broadcast ke ${chats.length} chat_`, m)
-  for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + author + ' All Chat Broadcast 」\n' + randomID(32)), true).catch(_ => _)
+  for (let id of chats) await conn.copyNForward(id, conn.cMods(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : teks + '\n' + readMore + '「 ' + author + ' All Chat Broadcast 」\n' + randomID(32)), true).catch(_ => _)
   m.reply('Selesai Broadcast All Chat :)')
 }
 handler.help = ['broadcastchats', 'bcchats'].map(v => v + ' <teks>')
