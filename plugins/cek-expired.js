43031f179e58e3242181eb4ad93b89e145bbf987
Aine <79745902+Aiinne@users.noreply.github.com>
2022-06-04 16:07:09 +0700

    Update cek-expired.js

--- plugins/cek-expired.js
+++ plugins/cek-expired.js
@@ -7,8 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     var jumlahHari = 86400000 * args[0]
     var now = new Date() * 1
     
-    conn.reply(m.chat, `*Expired Group:*
-${msToDate(global.db.data.chats[who].expired - now)}`, m)
+    conn.reply(m.chat, `*Expired Group:* ${msToDate(global.db.data.chats[who].expired - now)}`, m)
 }
 handler.help = ['cekexpired']
 handler.tags = ['group']
