7789f4adf6ac32415b99320e5a80a2784927009f
Aine <79745902+Aiinne@users.noreply.github.com>
2022-06-07 19:37:41 +0700

    Fix uppercase and lowercase

--- plugins/kisahnabi.js
+++ plugins/kisahnabi.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text, usedPrefix, command }) => {
      if (!text) throw `Masukan nama nabi\nExample: ${usedPrefix + command} adam`
-     let url = await fetch(`https://raw.githubusercontent.com/ZeroChanBot/Api-Freee/a9da6483809a1fbf164cdf1dfbfc6a17f2814577/data/kisahNabi/${text}.json`)
+     let url = await fetch(`https://raw.githubusercontent.com/Aiinne/scrape/main/data/kisahnabi/${text}.json`)
      let kisah = await url.json()
-     let hasil = ` Nabi : ${kisah.name}\nTanggal Lahir : ${kisah.thn_kelahiran}\nTempat Lahir : ${kisah.tmp}\nUsia : ${kisah.usia}\nKisah : ${kisah.description}`
+     let hasil = `Nabi : ${kisah.name}\nTanggal Lahir : ${kisah.thn_kelahiran}\nTempat Lahir : ${kisah.tmp}\nUsia : ${kisah.usia}\nKisah : ${kisah.description}`
      conn.reply(m.chat, hasil, m)
      }
 handler.help = ['kisahnabi <name>']
