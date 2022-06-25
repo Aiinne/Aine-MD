579cf9d60cc8e2fb055e3f3b6cd519cb2effc6d8
Zvael <77873451+Zvael@users.noreply.github.com>
2022-06-25 18:31:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = 'HambaAllah'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
