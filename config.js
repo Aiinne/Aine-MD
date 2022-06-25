900ae593ca6e6ee9e18041bceace5f2b26c81dfe
Zvael <77873451+Zvael@users.noreply.github.com>
2022-06-25 08:52:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6283879746467'],
+  ['6283879746467'],
+  ['6283879746467', 'Hamba Allah', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
