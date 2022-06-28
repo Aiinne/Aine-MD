ec4f5365f3bc9752b0d388661ee8974dce7abefc
zakixd12 <105186532+zakixd12@users.noreply.github.com>
2022-06-28 11:47:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6289506219231'],
+  ['6289506219231'],
+  ['6289506219231', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = [6289506219231] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
