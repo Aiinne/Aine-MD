6ae6c81aeb22abd14c2e5a3993358518e8232e41
HodeGTPS <108233508+HodeGTPS@users.noreply.github.com>
2022-06-29 05:55:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6289653599568'],
+  ['6289653599568'],
+  ['6289653599568', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
