5f840efa6b94e0839324b401ee5ff51368ca4e60
zakixd12 <105186532+zakixd12@users.noreply.github.com>
2022-06-28 11:53:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ global.owner = [
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [6289506219231] // Premium user has unlimited limit
+global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
