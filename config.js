34de4092686925995e648e387759be1cfb5ac307
tohiddos <94755267+tohiddos@users.noreply.github.com>
2022-07-02 09:57:57 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['989021155280'],
+  ['989021155280'],
+  ['989021155280', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
