487aa5564a9f0cda4cb83d84b3e26695cdd0e5e3
ZERO-ZEZ <108684669+ZERO-ZEZ@users.noreply.github.com>
2022-07-11 21:31:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6283178502766'],
+  ['6283178502766'],
+  ['6283178502766', 'Owner-Zero', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
