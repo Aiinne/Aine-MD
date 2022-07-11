7d3336d36c0e4f315c8a18b706ccc296db70515d
NurFy <86988258+NurFy@users.noreply.github.com>
2022-07-11 07:49:06 +0700

    Update tagme.js
    rapihin aja

--- plugins/tagme.js
+++ plugins/tagme.js
@@ -1,21 +1,10 @@
-let handler = async (m, { conn, text }) => {
-
-let orang = (await conn.groupMetadata(m.chat)).participants.map(u => u.jid)
-
-let tag = `@${m.sender.replace(/@.+/, '')}`
-
-let mentionedJid = [m.sender]
-
-  conn.reply(m.chat, tag, m, { contextInfo: { mentionedJid }})
-
+let handler = async (m) => {
+  m.reply(`@${m.sender.split`@`[0]}`)
 }
 
 handler.help = ['tagme']
-
 handler.tags = ['group']
-
 handler.command = /^tagme$/i
-
 handler.group = true
 
 module.exports = handler
