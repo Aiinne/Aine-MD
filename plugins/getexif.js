5084ca9ba207e15dacec08a40f8218c5a3bc4a2a
Aine <79745902+Aiinne@users.noreply.github.com>
2022-07-15 06:12:36 +0700

    Nambahin fitur biar betah

---
+++ plugins/getexif.js
@@ -0,0 +1,19 @@
+let webp = require('node-webpmux')
+let util = require('util')
+
+let handler = async (m) => {
+    if (!m.quoted) return m.reply('Tag stikernya!')
+    let q = { message: { [m.quoted.mtype]: m.quoted } }
+    if (/sticker/.test(m.quoted.mtype)) {
+        let img = new webp.Image()
+        await img.load(await m.quoted.download())
+        m.reply(util.format(JSON.parse(img.exif.slice(22).toString())))
+    }
+}
+handler.help = ['getexif']
+handler.tags = ['sticker']
+
+handler.command = ['getexif']
+handler.limit = true
+
+module.exports = handler
