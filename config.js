2eb9073b29b5fd29eade0bc4a79c650d8b54ff11
Hanafi1235678 <92976293+Hanafi1235678@users.noreply.github.com>
2022-07-31 21:58:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6285707627471'],
+  ['6283833584804'],
+  ['6281703100723', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 mods = [] // Want some help?
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 packname = sticker_name
 author = sticker_author
-wm = '© Aine'
+wm = '© Hanafi'
 
 multiplier = 1000 // The higher, The harder levelup
 
