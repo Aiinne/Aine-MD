c35f9dba56bdd128eba4c81fca759648303fe917
Aine <79745902+Aiinne@users.noreply.github.com>
2022-08-22 01:24:11 +0700

    Delete global

--- plugins/ttp.js
+++ plugins/ttp.js
@@ -3,7 +3,7 @@ const { MessageType } = require('@adiwajshing/baileys')
 
 let handler = async (m, { conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), global.packname, global.author)
+  let stiker = await sticker(null, global.API('xteam', '/ttp', { file: '', text: teks }), packname, author)
   if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m, false, { asSticker: true })
   throw stiker.toString()
 }
