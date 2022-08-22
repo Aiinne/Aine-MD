e7749a497b15a7d0a9b76664e5e8c689e65cc48b
TimzzOfc <timybot43@gmail.com>
2022-08-22 20:17:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ let chalk = require('chalk')
 
 owner = [
   ['62895330379186'],
-  ['62895330379186'],
+  ['6289533pp79186'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
