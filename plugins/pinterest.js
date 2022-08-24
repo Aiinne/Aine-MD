ee5b60cc0e8f733e4a4449e8a7b5ea00af3befda
The.sad.boy01 <88497118+Kangsad01@users.noreply.github.com>
2022-08-24 21:24:41 +0700

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,26 +1,3 @@
-/*let fetch = require('node-fetch')
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Contoh: ${usedPrefix + command} minecraft`
-  let res = await fetch(global.API('zeks', '/api/pinimg', {
-    q: encodeURI(text)
-  }, 'apikey'))
-  if (!res.ok) throw await `${res.status} ${res.statusText}`
-  let json = await res.json()
-  if (!json.status) throw json
-  let pint = json.data[Math.floor(Math.random() * json.data.length)];
-  conn.sendFile(m.chat, pint, '', `
-*Hasil pencarian*
-${text}
-`.trim(), m)
-}
-handler.help = ['pinterest <keyword>']
-handler.tags = ['internet']
-handler.command = /^(pinterest)$/i
-handler.limit = true
-
-module.exports = handler*/
-
-
 let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
