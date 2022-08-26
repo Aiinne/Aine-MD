6212ac38b8ff9ac0442dd8a98e8f23c9b3e35dcc
fahz-mdd <112198032+fahz-mdd@users.noreply.github.com>
2022-08-26 22:01:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,63 +1,113 @@
 let fs = require('fs') 
+
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+
+  ['+967771550103'],
+
+  ['+967771550103'],
+
+  ['+967771550103', 'Owner', true]
+
   // [number, dia creator/owner?, dia developer?]
+
 ] // Put your number here
+
 mods = [] // Want some help?
+
 prems = [] // Premium user has unlimited limit
+
 APIs = { // API Prefix
+
   // name: 'https://website'
+
   nrtm: 'https://nurutomo.herokuapp.com',
+
   xteam: 'https://api.xteam.xyz',
+
   zahir: 'https://zahirr-web.herokuapp.com',
+
   bcil: 'https://75.119.137.248:21587',
+
   neoxr: 'https://api.neoxr.eu.org/',
+
   zeks: 'https://api.zeks.me',
+
   gimez: 'https://masgimenz.my.id/',
+
   melcanz: 'https://melcanz.com',
+
   pencarikode: 'https://pencarikode.xyz',
+
   LeysCoder: 'https://leyscoders-api.herokuapp.com',
+
   restapi: 'https://x-restapi.herokuapp.com'
+
 }
+
 APIKeys = { // APIKey Here
+
   // 'https://website': 'apikey'
+
   'https://api.xteam.xyz': 'apikeyaine',
+
   'https://zahirr-web.herokuapp.com': 'zahirgans',
+
   'https://api.neoxr.eu.org/': 'jVEMyB2ITJ',
+
   'https://api.zeks.me': 'apikeyaine',
+
   'https://pencarikode.xyz': 'pais',
+
   'https://melcanz.com': 'ZZBk7EBb',
+
   'https://leyscoders-api.herokuapp.com': 'dappakntlll',
+
   'https://x-restapi.herokuapp.com': 'BETA'
+
 }
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 
 const spack = fs.readFileSync("lib/exif.json")
+
 const stickerpack = JSON.parse(spack)
+
 if (stickerpack.spackname == '') {
+
   var sticker_name = '@ainee_bot'
+
   var sticker_author = 'Aine'
+
 } else {
+
   var sticker_name = stickerpack.spackname
+
   var sticker_author = stickerpack.sauthor
+
 }
 
 const file_exif = "lib/exif.json"
+
 fs.watchFile(file_exif, () => {
+
   fs.unwatchFile(file_exif)
+
   console.log(chalk.redBright("Update 'exif.json'"))
+
   delete require.cache[file_exif]
+
   require('./lib/exif.json')
+
 })
 
 // Sticker WM
+
 packname = sticker_name
+
 author = sticker_author
+
 wm = '© Aine'
 
 Intervalmsg = 1800 //detik
@@ -65,43 +115,81 @@ Intervalmsg = 1800 //detik
 multiplier = 1000 // The higher, The harder levelup
 
 rpg = {
+
   emoticon(string) {
+
     string = string.toLowerCase()
+
     let emot = {
+
       exp: '✉️',
+
       money: '💵',
+
       potion: '🥤',
+
       diamond: '💎',
+
       common: '📦',
+
       uncommon: '🎁',
+
       mythic: '🗳️',
+
       legendary: '🗃️',
+
       pet: '🎁',
+
       trash: '🗑',
+
       armor: '🥼',
+
       sword: '⚔️',
+
       wood: '🪵',
+
       rock: '🪨',
+
       string: '🕸️',
+
       horse: '🐎',
+
       cat: '🐈' ,
+
       dog: '🐕',
+
       fox: '🦊',
+
       petFood: '🍖',
+
       iron: '⛓️',
+
       gold: '👑',
+
       emerald: '💚'
+
     }
+
     let results = Object.keys(emot).map(v => [v, new RegExp(v, 'gi')]).filter(v => v[1].test(string))
+
     if (!results.length) return ''
+
     else return emot[results[0][0]]
+
   }
+
 }
 
 let file = require.resolve(__filename)
+
 fs.watchFile(file, () => {
+
   fs.unwatchFile(file)
+
   console.log(chalk.redBright("Update 'config.js'"))
+
   delete require.cache[file]
+
   require(file)
+
 })
