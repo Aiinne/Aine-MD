b8e8f2aff4382ee28a602b7d85d36195040d5687
Aine <79745902+Aiinne@users.noreply.github.com>
2022-08-27 01:11:04 +0700

    Update o-cheat.js

--- plugins/o-cheat.js
+++ plugins/o-cheat.js
@@ -1,5 +1,5 @@
 let { MessageType } = require('@adiwajshing/baileys')
-let handler  = async (m, { conn, command, args, usedPrefix, isOwner, DevMode }) => {
+let handler  = async (m, { conn, command, args, usedPrefix:p, isOwner, DevMode }) => {
     if (!isOwner) throw 'Ngapain Make Cheat ???'
     let type = (args[0] || '').toLowerCase()
     let cht = (args[0] || '').toLowerCase()
@@ -8,24 +8,24 @@ let handler  = async (m, { conn, command, args, usedPrefix, isOwner, DevMode }) 
     let cok = `
 *[ LIST CHEAT  ]*
 • Exp\n• Level\n• Limit
-• .cheat exp 10 @62xx
+• ${p}cheat exp 10 @628123456789
 `.trim()
 try {
        if (/cheat/i.test(command)) {
             const count = args[1] && args[1].length > 0 ? Math.max(parseInt(args[1]), 1) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
             switch (type) {
             	case 'exp':
-                            if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
+                            if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam database'
                             db.data.users[who].exp += count * 1
                             conn.reply(m.chat, `Berhasil menambahkan ${count * 1} ${type} kedalam database target`, m)
 break                       // Perhatian Cheat exp bisa membuat database kalian error!!
                             // Minimal cheat exp 1000000000000 Batas cheat exp tidak boleh lebih!!
                 case 'level':
-                            if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
+                            if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam database'
                             db.data.users[who].level += count * 1
                             conn.reply(m.chat, `Berhasil menambahkan ${count * 1} ${type} kedalam database target`, m)                      
                 case 'limit':
-                            if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam data base'
+                            if (typeof db.data.users[who] == 'undefined') throw 'Pengguna tidak ada didalam database'
                             db.data.users[who].limit += count * 1
                             conn.reply(m.chat, `Berhasil menambahkan ${count * 1} ${type} kedalam database target`, m)
 break
@@ -34,12 +34,12 @@ break
             }
         }
     } catch (e) {
-        conn.reply(m.chat, `Done`, m)
+        conn.reply(m.chat, cok, m)
         console.log(e)
     }
 }
 
-handler.help = ['cheat <jumlah>', 'cheat <jumlah> @user']
+handler.help = ['cheat <type> <jumlah> @user']
 handler.tags = ['owner']
 handler.command = /^(cheat)$/i
 handler.fail = null
