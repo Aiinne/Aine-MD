49f625ac560de455d61c8ce93a72ba3317b6633c
乂Nat <83612604+Natx-C@users.noreply.github.com>
2022-08-29 19:16:21 +0700

    delete

--- plugins/delete.js
+++ plugins/delete.js
@@ -1,4 +1,4 @@
-let handler = function (m) {
+/*let handler = function (m) {
     /*if (!m.quoted) throw false
     let { chat, fromMe, isBaileys } = m.quoted
     if (!fromMe) throw false
@@ -16,4 +16,26 @@ handler.tags = ['tools']
 handler.command = /^del(ete)?$/i
 handler.limit = true
 
+module.exports = handler
+*/
+let handler = function (m) {
+  //if (!m.quoted) throw false
+ //let { chat, fromMe, id, isBaileys } = m.quoted
+ // if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+ let key = {}
+ try {
+ 	key.remoteJid = m.quoted ? m.quoted.fakeObj.key.remoteJid : m.key.remoteJid
+	key.fromMe = m.quoted ? m.quoted.fakeObj.key.fromMe : m.key.fromMe
+	key.id = m.quoted ? m.quoted.fakeObj.key.id : m.key.id
+ 	key.participant = m.quoted ? m.quoted.fakeObj.participant : m.key.participant
+ } catch (e) {
+ 	console.error(e)
+ }
+ conn.sendMessage(m.chat, { delete: key })
+}
+handler.help = ['delete']
+handler.tags = ['info']
+handler.command = /^(del|delete|unsend?)$/i
+handler.limit = false
+handler.admin = true
 module.exports = handler
