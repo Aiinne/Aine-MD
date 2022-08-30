4826c4758a824606d8972f0144526e0aee001090
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-30 17:31:08 +0700

    Update membunuh.js

--- plugins/membunuh.js
+++ plugins/membunuh.js
@@ -1,8 +1,8 @@
 
 // let pajak = 0.02
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-let dapat = (Math.floor(Math.random() * 100000000000000000000000))
-let healtu = (Math.floor(Math.random() * 10000))
+let dapat = (Math.floor(Math.random() * 10000))
+let healtu = (Math.floor(Math.random() * 100))
 let nomors = m.sender
   let who
   if (m.isGroup) who = m.mentionedJid[0]
