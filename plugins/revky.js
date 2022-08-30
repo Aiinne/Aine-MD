5f37436e497b0bbc4aa061d211409706b7594e0b
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-30 14:15:01 +0700

    Update revky.js

--- plugins/revky.js
+++ plugins/revky.js
@@ -1,10 +1,8 @@
-let handler = async m => m.reply(`
-'iya ka, ada apa?'
-`.trim()) 
+let handler = async m => m.reply(`iya ka, ada apa?`.trim()) 
 
 handler.help = ['revky']
 handler.tags = ['revky']
-handler.command = /^revky$/i
+handler.command = /^(revky)$/i
 handler.limit = true
 handler.group = true
 module.exports = handler
