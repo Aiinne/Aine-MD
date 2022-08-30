a7205dd682d91332be926501c33c427970470e63
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-30 18:15:53 +0700

    Update revky.js

--- plugins/revky.js
+++ plugins/revky.js
@@ -1,16 +1,6 @@
-let fs = require('fs')
-const { MessageType } = require('@adiwajshing/baileys')
-let handler = async (m, { conn }) => {
-await 'conn.sendMessage(m.chat, { hai ka aku revky}, m)'
-}
-
-handler.customPrefix = /^(rev|revk|revky|rep|repii|repkii|revv|revki)$/i
-handler.command = new RegExp
-
-handler.limit = true
-handler.mods = false 
-handler.premium = false
-handler.group = false
-handler.private = false
+let handler = async m => m.reply(`kenapa ka?, aku revky`.trim()) // Tambah sendiri kalo mau
+handler.help = ['revky']
+handler.tags = ['revky']
+handler.command = /^(rev|revk|revky|repki|refki)/i
 
 module.exports = handler
