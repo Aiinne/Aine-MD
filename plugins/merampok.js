7ff2acadee352403c733bcccdc90dce39d9965b0
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-30 11:29:40 +0700

    Update merampok.js

--- plugins/merampok.js
+++ plugins/merampok.js
@@ -1,7 +1,7 @@
 
 // let pajak = 0.02
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-let dapat = (Math.floor(Math.random() * 10000000000))
+let dapat = (Math.floor(Math.random() * 100000000000000000))
 let nomors = m.sender
   let who
   if (m.isGroup) who = m.mentionedJid[0]
