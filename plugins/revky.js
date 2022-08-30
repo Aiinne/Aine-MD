1a5ee67831168aa950d0ace644287f37be563d2b
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-30 17:41:22 +0700

    Update revky.js

--- plugins/revky.js
+++ plugins/revky.js
@@ -4,7 +4,4 @@ let handler = async m => m.reply(`iya ka, ada apa?`.trim())
 handler.customPrefix = /^(revky|re|rev|revk|revky|rep|repk|repki|refky|revki)$/i
 handler.limit = true
 handler.group = true
-    }
-    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    conn.sendButton(m.chat, text.trim(), 'Made with ♡ RevkyRama!', null, [['iya, ada apa ka?', '.Iya, Ada Apa Ka'], m)
 module.exports = handler
