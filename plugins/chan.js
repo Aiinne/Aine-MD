7715cbea1fc3725d98717068bb1cc03ed63782d5
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-30 14:30:26 +0700

    Delete chan.js

--- plugins/chan.js
+++
@@ -1,9 +0,0 @@
-let handler = async m => m.reply('PENGEN CURI YA?!`.trim())
-
-
-handler.help = ['revky']
-handler.tags = ['revky']
-handler.command = /^(revky)$/i
-handler.limit = true
-handler.group = true
-module.exports = handler
