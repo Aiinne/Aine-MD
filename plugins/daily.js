fbe34a4dc71b1f5c21400aa3f4977b658c74fa3b
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-30 20:12:34 +0700

    Update daily.js

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,4 +1,4 @@
-const free = 1000000000
+const free = 1000000
 const prem = 10000
 const moneyfree = 10000000
 const moneyprem = 10000
@@ -11,7 +11,7 @@ let handler = async (m, { conn, isPrems }) => {
         global.db.data.users[m.sender].exp += isPrems ? prem : free
         global.db.data.users[m.sender].money += isPrems ? moneyprem : moneyfree
        // global.db.data.users[m.sender].potion += 5
-        conn.reply(m.chat, `hey owner, take revky~:\n\n+${isPrems ? prem : free} Exp\n+${isPrems ? moneyprem : moneyfree} Money`, m)
+        conn.reply(m.chat, `Selamat kamu mendapatkan:\n\n+${isPrems ? prem : free} Exp\n+${isPrems ? moneyprem : moneyfree} Money`, m)
         global.db.data.users[m.sender].lastclaim = new Date * 1
         setTimeout(() => {
 					conn.reply(m.chat, `Daily sudah bisa di dapatkan kembali`, m)
@@ -20,14 +20,14 @@ let handler = async (m, { conn, isPrems }) => {
 handler.help = ['daily']
 handler.tags = ['rpgabsen']
 handler.command = /^(daily)$/i
-handler.owner = true
+handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
 
-handler.admin = true
-handler.botAdmin = true
+handler.admin = false
+handler.botAdmin = false
 
 handler.fail = null
 handler.money = 0
@@ -37,15 +37,15 @@ handler.limit = true
 module.exports = handler
 
 function msToTime(duration) {
-  var milliseconds = parseInt((duration % 0) / 0),
-    seconds = Math.floor((duration / 0) % 60),
-    minutes = Math.floor((duration / (0 * ))) % 60),
-    hours = Math.floor((duration / (0 *  * 0)) % 24)
+  var milliseconds = parseInt((duration % 1000) / 100),
+    seconds = Math.floor((duration / 1000) % 60),
+    minutes = Math.floor((duration / (1000 * 60)) % 60),
+    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
     
   
-  hours = (hours < 0) ? "0" + hours : hours
-  minutes = (minutes < 0) ? "0" + minutes : minutes
-  seconds = (seconds < 0) ? "0" + seconds : seconds
+  hours = (hours < 10) ? "0" + hours : hours
+  minutes = (minutes < 10) ? "0" + minutes : minutes
+  seconds = (seconds < 10) ? "0" + seconds : seconds
 
   return hours + " jam " + minutes + " menit " + seconds + " detik"
 }
