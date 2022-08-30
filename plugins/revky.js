99af9d195c036f0e481e56675698d64b51e8c2e9
revkykira <100487320+revkykira@users.noreply.github.com>
2022-08-30 17:28:13 +0700

    Update revky.js

--- plugins/revky.js
+++ plugins/revky.js
@@ -6,4 +6,7 @@ handler.command = /^(revky)$/i
 handler.customPrefix = /^(revky|re|rev|revk|revky|rep|repk|repki|refky|revki)$/i
 handler.limit = true
 handler.group = true
+    }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    conn.sendButton(m.chat, text.trim(), 'Made with ♡ RevkyRama!', null, [['iya, ada apa ka?', '.Iya, Ada Apa Ka'], m)
 module.exports = handler
