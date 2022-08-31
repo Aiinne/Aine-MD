73df3db73f7642db4c018d5228daa78b3c99895d
revkykira <100487320+revkykira@users.noreply.github.com>
2022-09-01 02:42:57 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,1 +1,12 @@
+let handler = async m => m.reply(`
+╭─「 Donasi • Pulsa 」
+│ •  [6281802857821]
+│ •  [6281802857821]
+╰────
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
 
