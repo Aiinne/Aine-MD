0c385984c466b18ef5d8595d77f1e6dd6c5a828e
Lana3457 <119503041+Lana3457@users.noreply.github.com>
2022-11-30 19:59:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 owner = [
-  ['62895330379186'],
-  ['62895330379186'],
+  ['6282138101989'],
+  ['6282138101989'],
   ['62895330379186', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
